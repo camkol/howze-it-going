0ba4a3d8aa1e74649856f786cb6badcb163d9da5
camkol <koliyus3@hotmail.com>
2024-05-03 18:46:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  return <Navigation />;
+}
+
+function Navigation() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {" "}
+      <header>
+        <nav className="navbar" role="navigation">
+          <a className="roll-out-text" href="index.html">
+            <h1 id="top">Cameron Howze</h1>
+          </a>
+          <button
+            className="navbarToggler"
+            type="button"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbarTogglerIcon" />
+          </button>
+          <div id="navbarSupportedContent" className="collapse navbar-collapse">
+            <ul className="navbarNav">
+              <li className="navItem">
+                <a className="navLink active" href="index.html">
+                  <h4>Home</h4>
+                </a>
+              </li>
+              <li className="navItem">
+                <a className="navLink" href="#education">
+                  <h4>Education</h4>
+                </a>
+              </li>
+              <li className="navItem">
+                <a className="navLink" href="#experience">
+                  <h4>Employment</h4>
+                </a>
+              </li>
+              <li className="navItem">
+                <a className="navLink" href="about.html">
+                  <h4>About Me</h4>
+                </a>
+              </li>
+              <li className="navItem">
+                <a className="navLink" href="library.html">
+                  <h4>Library</h4>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
-    </div>
+    </>
   );
 }
 
