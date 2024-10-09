b65aee36281256fc335262c11673ac5278a9b5fd
camkol <121874225+camkol@users.noreply.github.com>
2024-10-09 16:28:32 +0200

    set preview

--- src/Projects/Preview.js
+++ src/Projects/Preview.js
@@ -1,13 +1,13 @@
 export default function Preview() {
   return (
     <div className="previewer">
-      <div className="previewer-dialog">
-        <div className="previewer-content">
+      <div className="previewer-image">
+        <img src="/images/projects/xyline-site.JPG" alt="xyline" />
+      </div>
+      <hr />
+      <div className="previewer-content">
+        <div className="previewer-dialog">
           <div className="previewer-links">
-            <div className="previewer-image">
-              <img src="/images/projects/xyline-site.JPG" alt="xyline" />
-            </div>
-            <hr />
             <h5>
               <a
                 className="card-link"
@@ -30,25 +30,24 @@ export default function Preview() {
               </a>
             </h4>
           </div>
-
-          <div className="previewer-process">
-            <h4>Process</h4>
-            <ul>
-              <li>
-                {"I undertook the development for a close friend's company."}
-              </li>
-              <li>{`Striving for consistency, I emulated the design of their old
+        </div>
+        <div className="previewer-process">
+          <h4>Process</h4>
+          <ul>
+            <li>
+              {"I undertook the development for a close friend's company."}
+            </li>
+            <li>{`Striving for consistency, I emulated the design of their old
 website, then enhance upon request.`}</li>
-              <li>
-                {"Bootstrap was incorporated to enhance structural elements."}
-              </li>
-              <li>{`A JavaScript-based slideshow was implemented for visual
+            <li>
+              {"Bootstrap was incorporated to enhance structural elements."}
+            </li>
+            <li>{`A JavaScript-based slideshow was implemented for visual
 appeal.`}</li>
-              <li>{`Leveraging my Photoshop skills, I converted images to JPG
+            <li>{`Leveraging my Photoshop skills, I converted images to JPG
 format to enhance accessibility and reduce file sizes,
 subsequently improving load times.`}</li>
-            </ul>
-          </div>
+          </ul>
         </div>
       </div>
     </div>
